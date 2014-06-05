d35f5773ee9b3bea4c5c87cf91881fb540314faa
cappu <da.cappu@gmail.com>
2014-06-05 23:58:40 +0300

    renaming functions, dropping useless functions

--- src/api-io.cpp
+++ src/api-io.cpp
@@ -4,16 +4,6 @@
 namespace laskin
 {
     /**
-     * cr()
-     *
-     * Outputs new line into standard output stream.
-     */
-    BUILT_IN_FUNCTION(func_cr)
-    {
-        out << std::endl;
-    }
-
-    /**
      * emit(int)
      *
      * Outputs given integer as ASCII character into the standard output
@@ -26,36 +16,24 @@ namespace laskin
     }
 
     /**
+     * nl()
+     *
+     * Outputs new line into standard output stream.
+     */
+    BUILT_IN_FUNCTION(func_nl)
+    {
+        out << std::endl;
+    }
+
+    /**
      * print(any)
      *
-     * Outputs given object into the standard output stream.
+     * Outputs given object into the standard output stream with appended new
+     * line.
      */
     BUILT_IN_FUNCTION(func_print)
     {
-        out << stack.back();
-        stack.pop();
-    }
-
-    /**
-     * println(any)
-     *
-     * Outputs given object into the standard output stream with new line.
-     */
-    BUILT_IN_FUNCTION(func_println)
-    {
         out << stack.back() << std::endl;
-        stack.pop();
-    }
-
-    /**
-     * .(any)
-     *
-     * Outputs given value prefixed with a white space character into the
-     * standard output stream.
-     */
-    BUILT_IN_FUNCTION(func_dot)
-    {
-        out << ' ' << stack.back();
         stack.pop();
     }
 
@@ -63,11 +41,9 @@ namespace laskin
     {
         void initialize_io(interpreter* i)
         {
-            i->register_function("cr", "", func_cr);
             i->register_function("emit", "i", func_emit);
+            i->register_function("nl", "", func_nl);
             i->register_function("print", "?", func_print);
-            i->register_function("println", "?", func_println);
-            i->register_function(".", "?", func_dot);
         }
     }
 }
